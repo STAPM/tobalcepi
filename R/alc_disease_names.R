791856cb07beebc4b12bdcbdf8676409c1e58e35
Duncan Gillespie <duncan.gillespie@sheffield.ac.uk>
2020-11-25 12:10:58 +0000

    add reference for alc disease names

--- R/alc_disease_names.R
+++ R/alc_disease_names.R
@@ -1,16 +1,14 @@
 
 #' Names of alcohol-related diseases
 #'
-#'
+#' @importFrom Rdpack reprompt
 #'
 #' @docType data
 #'
-#' @format A data table
+#' @format A character vector containing the names of the 48 disease categories 
+#' of alcohol related diseases \insertCite{Angus2018;textual}{tobalcepi}.
 #'
-#' @source 
-#'
-#'
-#'
-#'
+#' @references
+#' \insertRef{Angus2018}{tobalcepi}
 #'
 "alc_disease_names"

b50cbb044adea1e188e1bf8b8b0524951f2583d9
Duncan Gillespie <duncan.gillespie@sheffield.ac.uk>
2020-12-28 10:22:39 +0000

    update disease_groups

--- R/tob_disease_names.R
+++ R/tob_disease_names.R
@@ -12,3 +12,20 @@
 #' \insertRef{webster2018risk}{tobalcepi}
 #'
 "tob_disease_names"
+
+
+#' Groupings of smoking-related diseases into disease types
+#'
+#' @importFrom Rdpack reprompt
+#'
+#' @docType data
+#'
+#' @format A data.table containing disease groupings following the scheme used in 
+#' the Royal College of Physicians report 'Hiding in Plain Sight' Chapter 3 \insertCite{RCP2018}{tobalcepi}. 
+#' Also includes a column of formatted disease names.
+#'
+#' @references
+#' \insertRef{RCP2018}{tobalcepi}
+#'
+"disease_groups"
+
